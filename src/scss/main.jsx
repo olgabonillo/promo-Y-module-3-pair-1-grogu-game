404590e86617cb4fc8d316e58b958895629b3570
Olga <olgaalaquas@gmail.com>
2024-07-23 11:17:01 +0200

    created new play

---
+++ src/scss/main.jsx
@@ -0,0 +1,10 @@
+import "../components/App";
+import App from "../components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
